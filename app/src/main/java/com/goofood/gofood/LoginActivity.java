ee5a73ca58f7cb539f96f1b43dfb9cce82bb96e9
TJ <ranjeewa_tj@yahoo.com>
2016-05-14 11:11:55 -0400

    Adding GoFood Android Test app

---
+++ app/src/main/java/com/goofood/gofood/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.goofood.gofood;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tj on 5/13/2016.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+
+
+
+}
